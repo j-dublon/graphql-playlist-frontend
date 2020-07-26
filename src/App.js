6bfd2082dde349ae8d331738689d67aa84439db5
j-dublon <j.johnson.dublon@gmail.com>
2020-07-26 11:45:41 +0100

    configure apollo client

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import "./App.css";
+import BookList from "./components/BookList";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App__header">
-        <img
-          src={require("./logo.png")}
-          className="App__header__logo"
-          alt="logo"
-        />
-        <h1>GraphQl Playlist</h1>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <header className="main__header">
+          <img
+            src={require("./logo.png")}
+            className="main__header__logo"
+            alt="logo"
+          />
+          <h1>Jodi's Book List</h1>
+        </header>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/components/BookList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="booklist">
+          <li>Book 1</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
