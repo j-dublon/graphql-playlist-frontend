b2b939866bcd56bac1fc5aa61f705f29778113a3
j-dublon <j.johnson.dublon@gmail.com>
2020-07-27 11:05:25 +0100

    add addBookMutation

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,27 +1,37 @@
 import React, { Component } from "react";
-import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
+import { getAuthorsQuery, addBookMutation } from "../queries/queries";
+import * as compose from "lodash.flowright";
 
 class AddBook extends Component {
+  state = {
+    name: "",
+    genre: "",
+    authorId: "",
+  };
+
+  submitForm(event) {
+    event.preventDefault();
+  }
+
   render() {
-    const { data } = this.props;
+    const data = this.props.getAuthorsQuery;
     return (
-      <form className="addBook">
+      <form className="addBook" onSubmit={this.submitForm.bind(this)}>
         <label>Book Name</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => this.setState({ name: event.target.value })}
+        />
         <label>Genre</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => this.setState({ genre: event.target.value })}
+        />
         <label>Author</label>
-        <select>
+        <select
+          onChange={(event) => this.setState({ authorId: event.target.value })}
+        >
           {data.loading ? (
             <option disabled>Loading authors...</option>
           ) : (
@@ -40,4 +50,7 @@ class AddBook extends Component {
   }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+  graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
